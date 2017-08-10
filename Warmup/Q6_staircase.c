b1b45ab2d1ab9f8ecc6ecca239f376083e2ec3a1
SanchitThacker <sanchit.thacker@gmail.com>
2017-08-11 01:51:45 +0530

    question 6 uploaded

---
+++ Warmup/Q6_staircase.c
@@ -0,0 +1,26 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n,i,j,k; 
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<i;j++)
+        {
+            printf(" ");
+        }
+        for(k=n;k>=i;k--)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
